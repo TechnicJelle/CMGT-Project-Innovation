e39b4563988ef135d421fef92fee4981112ebcc9
mRmetr0 <99254814+mRmetr0@users.noreply.github.com>
2023-04-05 14:10:27 +0200

    Add forward boat movement, add immersive movement

--- BattleShips_Host/Assets/Scripts/Boat.cs
+++ BattleShips_Host/Assets/Scripts/Boat.cs
@@ -7,26 +7,27 @@ public class Boat : MonoBehaviour
     private Vector3 _direction;
     [SerializeField][Range(0, 360)]private float rotation;
     [SerializeField] private float moveSpeed;
-    [SerializeField] private float rotSpeed;
-    void Awake () {
+    [SerializeField] [Range(0, 0.1f)]private float rotSpeed;
+    [SerializeField] private bool go = false;
+
+    private void Awake () {
         _rb = GetComponent<Rigidbody>();
+        Debug.Log(_rb);
         _direction = Vector3.forward;
     }
 
-    void FixedUpdate()
+    private void FixedUpdate()
     {
         Move();
     }
 
-    void Move()
+    private void Move()
     {
         _direction = Quaternion.Euler(new Vector3(0, rotation, 0)) * Vector3.forward;
-        Debug.Log(_direction);
         var forward = transform.forward;
         forward = Vector3.Lerp(forward, _direction, rotSpeed);
         transform.forward = forward;
-        //transform.Rotate(new Vector3(0, rotation, 0));
-        _rb.MovePosition(forward * (moveSpeed * Time.deltaTime));
-        
+        if (go)
+            _rb.AddForce(forward * (moveSpeed * Time.fixedDeltaTime * 100));
     }
 }
