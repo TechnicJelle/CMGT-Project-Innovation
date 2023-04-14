a37a01933b09f76881a5960768d82e5ef7ec4d82
mRmetr0 <99254814+mRmetr0@users.noreply.github.com>
2023-04-14 16:21:02 +0200

    Add level layout, add boat texture, Add distance to ImmersiveCamera

--- BattleShips_Host/Assets/Scripts/ImmersiveCamera.cs
+++ BattleShips_Host/Assets/Scripts/ImmersiveCamera.cs
@@ -8,17 +8,25 @@ public class ImmersiveCamera : MonoBehaviour
 
     [SerializeField] private Vector3 offset;
     [SerializeField] private float smoothTime = 0.8f;
+    [Space]
     [SerializeField] private float minZoom = 100f;
     [SerializeField] private float maxZoom = 15f;
     [SerializeField] private float zoomLimiter = 70f;
-
+    [SerializeField] private bool lensZoom;
+    [Space] 
+    [SerializeField] private float maxDist;
+    [SerializeField] private float minDist;
+    [SerializeField] private float distLimiter;
+    
     private Vector3 _velocity;
+    private Vector3 _baseOffset;
     private Camera _camera;
     private Bounds _bounds;
 
     private void Awake()
     {
         _camera = GetComponent<Camera>();
+        _baseOffset = offset;
     }
 
     private void LateUpdate()
@@ -33,7 +41,10 @@ public class ImmersiveCamera : MonoBehaviour
         }
         
         Move();
-        Zoom();
+        if (lensZoom)
+            LensZoom();
+        else
+            DistZoom();
     }
 
     private void Move()
@@ -43,11 +54,17 @@ public class ImmersiveCamera : MonoBehaviour
         transform.position = Vector3.SmoothDamp(transform.position, newPos, ref _velocity, smoothTime);
     }
 
-    private void Zoom()
+    private void LensZoom()
     {
         float newZoom = Mathf.Lerp(maxZoom, minZoom, GetGreatestDistance()/zoomLimiter);
         _camera.fieldOfView = Mathf.Lerp(_camera.fieldOfView, newZoom, Time.deltaTime);
     }
+
+    private void DistZoom()
+    {
+        float newDist = Mathf.Lerp(maxDist, minDist, GetGreatestDistance()/distLimiter);
+        offset = _baseOffset * newDist;
+    }   
 
     private Vector3 GetCenterPoint()
     {
