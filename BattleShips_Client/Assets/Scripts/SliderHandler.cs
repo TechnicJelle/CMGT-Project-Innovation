3eb6c36cb66a871206c48ddcffd02f6f6b4e18cf
TechnicJelle <22576047+TechnicJelle@users.noreply.github.com>
2023-06-08 03:51:15 +0200

    Clean up & Fix Client UI

--- BattleShips_Client/Assets/Scripts/SliderHandler.cs
+++ BattleShips_Client/Assets/Scripts/SliderHandler.cs
@@ -3,6 +3,9 @@ using UnityEngine.EventSystems;
 
 public class SliderHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
 {
+	[SerializeField] private bool updateInEditor;
+	[Range(0f, 1f)] [SerializeField] private float offsetFactor = 0.4f;
+
 	public bool Pressed { get; private set; }
 
 	private void Awake()
@@ -10,8 +13,14 @@ public class SliderHandler : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
 		//ensure that the handle always actually fits fully on screen
 		RectTransform rectTransform = GetComponent<RectTransform>();
 		Vector3 rectTransformPosition = rectTransform.localPosition;
-		rectTransformPosition.y = Mathf.Min(Screen.width, Screen.height) * 0.3f;
+		rectTransformPosition.y = Mathf.Min(Screen.width, Screen.height) * offsetFactor;
 		rectTransform.localPosition = rectTransformPosition;
+	}
+
+	private void OnValidate()
+	{
+		if (updateInEditor)
+			Awake();
 	}
 
 	public void OnPointerDown(PointerEventData eventData)
